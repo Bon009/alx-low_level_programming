ff94568a72569ed85f7f5f96996512ba949ac0d2
Bon Enyeobi <bonvault@aol.com>
2022-10-13 21:10:26 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,3 +10,6 @@ int main(void)
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
+
