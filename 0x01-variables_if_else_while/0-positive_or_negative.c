af968b2d9747c9afbe30abcb449ab1be814badab
Bon Enyeobi <bonvault@aol.com>
2022-10-14 13:41:10 -0700

    C Variables Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+/**
  * main-program entry point.
  * Return: 0-no error,non zero value if error.
  */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
