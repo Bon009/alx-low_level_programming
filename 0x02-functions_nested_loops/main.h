6afb24ca3a298218e6e3777718c337b3cbe59337
Bon Enyeobi <bonvault@aol.com>
2022-10-18 10:08:00 -0700

    C - functions Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,6 +11,6 @@ void jack_bauer(void);
 void times_table(void);
 int add((int, int);
 void print_to_98(int n);
-int _putchar(char c);
+int _putchar(char c)
 
 #endif
