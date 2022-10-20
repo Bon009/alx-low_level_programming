d760b790d7935b02d5cdfd5fd30d988b4c2dbb71
Bon Enyeobi <bonvault@aol.com>
2022-10-20 15:18:12 -0700

    C Debugging Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,10 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_reaining_days(int month, int day, int year);
-
 #endif
