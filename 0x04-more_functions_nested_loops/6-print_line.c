67552ba64bb0d66293e9bbf1fe3fd9d032453b2d
Bon Enyeobi <bonvault@aol.com>
2022-10-20 17:38:56 -0700

    C more functions Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * more_numbers - nmbr fct
- * Return: no return
+ * print_line - printing lines
+ * @n: number of times
+ * Return: void
  */
 
-void more_numbers(void)
-{
 
-	int i, j;
-	for (i = 1; i <= 10; i++)
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar(j % 10 + '0');
-		}
-		_putchar('\n');
+		_putchar('_');
 	}
+	_putchar('\n');
 }
