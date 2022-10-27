87e02f66b8141829bb78d1ea9a6de2853324fc22
Bon Enyeobi <bonvault@aol.com>
2022-10-27 13:41:00 -0700

    C pointer, arrays and string 0x06 advanced Task 0 to 4

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void)
+int main(void)
 {
-	int n;
+	int n;
 	int a[5];
 	int *p;
 
@@ -11,13 +11,13 @@ int main(void)
 	/*
 	 * write your line of code here...
 	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
