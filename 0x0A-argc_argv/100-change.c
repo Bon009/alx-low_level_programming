f18895b053e9e0b83576b7f122ca69ee20676623
Bon Enyeobi <bonvault@aol.com>
2022-11-04 21:58:49 -0700

    C Argc and Argv Adavanced Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,40 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - adds positive numbers.
- * @argc: argument count
- * @argv: arguments
- *
- * Return: 0
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: holds the number of arguements passed
+ * @argv: the array pointer that holds the arguement passed
+ * Return: always 0 (success)
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, n, sum = 0;
-	char *flag;
-
-	if (argc < 2)
+	int cents, coins_number = 0;
+	if (argc != 2)
 	{
-		printf("0\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-
-	for (i = 1; argv[i]; i++)
+	else
 	{
-		n = strtol(argv[i], &flag, 10);
-		if (*flag)
+		cents = atoi(argv[1]);
+		while (cents > 0)
 		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			sum += n;
+			if (cents >= 25)
+				cents -= 25;
+			else if (cents >= 10)
+				cents -= 10;
+			else if (cents >= 5)
+				cents -= 5;
+			else if (cents >= 2)
+				cents -= 2;
+			else if (cents >= 1)
+				cents -= 1;
+			coins_number += 1;
 		}
 	}
-	printf("%d\n", sum);
-
+	printf("%d\n", coins_number);
 	return (0);
 }
