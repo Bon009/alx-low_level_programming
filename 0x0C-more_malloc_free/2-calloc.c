59e03e61b1e18954c42003d74bffc4fe0e6eb4f7
Bon Enyeobi <bonvault@aol.com>
2022-11-10 18:41:31 -0800

    C more malloc_free Project Task 0 to 3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,7 +6,6 @@
  *
  * @nmemb: allocate memory for array
  * @size: allocate element of size bytes
- *
  * Return: pointer to the allocated memory.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -16,10 +15,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-       a = malloc(nmemb * size);
-       if (a == NULL)
-	       return (NULL);
-       for (b = 0; b < (nmemb * size); b++)
-	       a[b] = 0;
-       return (a);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (b = 0; b < (nmemb * size); b++)
+		a[b] = 0;
+	return (a);
 }
