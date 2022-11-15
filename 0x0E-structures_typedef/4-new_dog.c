43737a4ab7f12aef6db8b8af9c04b242c8baa273
Bon Enyeobi <bonvault@aol.com>
2022-11-14 22:33:47 -0800

    C structure project Task 0 to 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,7 +4,6 @@
 /**
  * new_dog - creates a new dog
  * @name: name of dog
- *
  * @age: age of dog
  * @owner: owner of dog
  * Return: pointer to new dog
@@ -13,7 +12,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
-
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
@@ -21,27 +19,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	for (nl = 0; name[nl]; nl++)
 		;
-	nl++job
-		dog->name = malloc(nl * sizeof(char));
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-											for (i = 0; i < nl; i++)
-												dog->name[i] = name[i];
-											dog->age = age;
-											for (ol = 0; owner[ol]; ol++)
-												;
-											ol++;
-											dog->owner = malloc(ol * sizeof(char));
-											if (dog->owner == NULL)
-											{
-												free(dog->name);
-												free(dog);
-												return (NULL);
-											}
-											for (i = 0; i < ol; i++)
-													dog->owner[i] = owner[i];
-											return (dog);
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
 }
